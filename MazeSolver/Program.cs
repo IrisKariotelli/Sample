705214b1a8046c12e480d79216e3fd9838d3ddd0
Iris Kariotelli <iriskario+git@gmail.com>
2014-02-15 08:20:32 +0000

    First commit

---
+++ MazeSolver/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MazeSolver
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+
+	public class MazeTraverser{
+		public bool FreeAhead(){
+			return true;
+		}
+	}
+}
---
+++ MazeSolverTests/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using MazeSolver;
+
+namespace MazeSolverTests
+{
+	[TestFixture ()]
+	public class SampleTests
+	{
+		[Test ()]
+		public void PrintsHello ()
+		{
+			var traverser = new MazeTraverser ();
+			Assert.IsTrue(traverser.FreeAhead());
+
+		}
+	}
+}
+
